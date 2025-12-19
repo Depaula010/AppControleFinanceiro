46def0193953e55607b973110b03a121bb0a21f3
Rafael <rafaeldepaula0010@gmail.com>
2025-12-19 15:27:30 -0300

    commit

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,12 +1,11 @@
 # app/services/__init__.py
 """
 Modulo de services - logica de negocio da aplicacao.
+
+IMPORTANTE: Nao importar services aqui para evitar import circular!
+Os services devem ser importados diretamente onde necessarios:
+
+    from app.services import finance_service
+    from app.services import gemini_service
+    from app.services.notification_service import enviar_notificacao_whatsapp
 """
-
-from . import gemini_service
-from . import whatsapp_service
-
-__all__ = [
-    'gemini_service',
-    'whatsapp_service',
-]
